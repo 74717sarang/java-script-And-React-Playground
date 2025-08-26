c9b07b00daf28d4c0d1ec961a59cc4400d9ba1db
74717sarang <patilsarang13520@gmail.com>
2025-08-26 12:36:30 +0530

    update

---
+++ Day4/my-app/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Grettings from './Grettings.jsx'
+
+function App() {
+
+
+  return (
+    <>
+      <Grettings name="Sarang" />
+      <Grettings name="Sanket" />
+      <Grettings name="Sujay" />
+      <Grettings name="Sankalp" />
+      <Grettings name="Satyam" />
+    </>
+  )
+}
+
+export default App
